3b8dba291072f2e340a3a1aa8be7ac75703abfad
Julian Gruber <julian@juliangruber.com>
2013-07-21 19:53:47 +0200

    simplify

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var test = RegExp.prototype.test;
 
 var regs = {
   readOnly:
-    '^(get|(create)?((r|R)ead|(v|V)alue|(k|K)ey)Stream|is(Open|Closed))$',
+    /^(get|(create)?((r|R)ead|(v|V)alue|(k|K)ey)Stream|is(Open|Closed))$/,
   canDelete:
-    '^(del|batch)$',
+    /^(del|batch)$/,
   readable:
-    '^(create)?((r|R)ead|(v|V)alue|(k|K)ey)Stream$',
+    /^(create)?((r|R)ead|(v|V)alue|(k|K)ey)Stream$/,
   writable:
-    '^(create)?(w|W)riteStream',
+    /^(create)?(w|W)riteStream$/,
   async:
-    '^(open|close|put|get|del|batch)$',
+    /^(open|close|put|get|del|batch)$/,
   sync:
-    '^is(Open|Closed)$'
+    /^is(Open|Closed)$/
 }
 
 module.exports = Object.keys(regs).reduce(function (acc, check) {
-  acc[check] = test.bind(new RegExp(regs[check]));
+  acc[check] = test.bind(regs[check]);
   return acc;
 }, {});
 
